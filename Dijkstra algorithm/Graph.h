b75414e0ff3a5583841a2820ea123430cf71fdaa
bae haram <gkfkagkfka12@naver.com>
2018-05-14 19:31:17 +0900

    Remove <stdio.h> and remove max initialization

--- Dijkstra algorithm/Graph.h
+++ Dijkstra algorithm/Graph.h
@@ -1,10 +1,9 @@
 #ifndef _GRAPH_H_
 #define _GRAPH_H_
 
-#include <stdio.h>
 // vertex는 A(0)부터 시작한다.
 enum { A, B, C, D, E, F, G, H, I, J, K };
-const int max = 2100000000;
+const int max;
 typedef enum { false, true } bool;
 
 // Vertex는 정점의 번호(혹은 알파벳)와 다음 정점으로의 포인터변수 next를 가진다.
