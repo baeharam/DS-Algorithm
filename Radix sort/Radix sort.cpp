a056bb6fae22fa54616c99f8e6f321e4c2746e40
bae haram <gkfkagkfka12@naver.com>
2018-06-01 00:26:47 +0900

    Code radix sort of cpp

--- Radix sort/Radix sort.cpp
+++ Radix sort/Radix sort.cpp
@@ -1,47 +1,32 @@
 #include <iostream>
-#include <queue>
+using namespace std;
+
+void RADIX_SORT(int A[], int d, int n)
+{
+	int *C = new int[10];
+	int *B = new int[n+1];
+	for (int i = 1; i <= n; i++) B[i] = 0;
+
+	for (int i = 1; i <= d; i++) {
+		int p = 1;
+		for (int j = 1; j <= i - 1; j++) p *= 10;
+		for (int j = 0; j <= 9; j++) C[j] = 0;
+		for (int j = 1; j <= n; j++) C[(A[j] / p) % 10]++;
+		for (int j = 1; j <= 9; j++) C[j] = C[j - 1] + C[j];
+		for (int j = n; j >= 1; j--) {
+			B[C[(A[j] / p) % 10]] = A[j];
+			C[(A[j] / p) % 10]--;
+		}
+		for (int j = 1; j <= n; j++) A[j] = B[j];
+	}
+
+	delete C, B;
+}
 
 int main(void)
 {
-	int arr[8] = { 170, 45, 75, 90, 2, 24, 802, 66 };
-	std::queue<int> radix[10]; // 자리수에 대한 큐 배열
-
-	int max = arr[0];
-	int d = 1; // 최대자리수
-
-	// 최대 자리수를 구하기 위해서 최댓값을 찾아낸다.
-	for (int i = 1; i < 8; i++)
-		if (max < arr[i]) max = arr[i];
-
-	// 최대 자리수를 구해낸다.
-	while (max/10){
-		d *= 10;
-		max /= 10;
-	}
-
-	int mod = 10;
-	int dMin = 1;
-
-	while (dMin<=d){
-		// 자리수에 따라 큐에 집어넣는다.
-		for (int i = 0; i < 8; i++){
-			radix[(arr[i] % mod)/dMin].push(arr[i]);
-		}
-
-		// 큐에 들어간 값들을 자리수 크기 순으로 다시 배열에 집어넣는다.
-		for (int i = 0, j = 0; i < 10;){
-			if (radix[i].size()){
-				arr[j++] = radix[i].front();
-				radix[i].pop();
-			}
-			else i++;
-		}
-
-		dMin *= 10;
-		mod *= 10;
-	}
-
-	for (int i = 0; i < 8; i++) std::cout << arr[i] << ' ';
-
+	int A[8] = { 0,329,457,657,839,436,720,355 };
+	RADIX_SORT(A, 3, 7);
+	for (int i = 1; i <= 7; i++) printf("%d ", A[i]);
 	return 0;
 }
