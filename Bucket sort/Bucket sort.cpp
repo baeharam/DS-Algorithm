bb7f0f19beed271759a34606b010cc63d933838f
bae haram <gkfkagkfka12@naver.com>
2018-06-12 22:36:54 +0900

    Recode by CLRS pseudo code

--- Bucket sort/Bucket sort.cpp
+++ Bucket sort/Bucket sort.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
+using namespace std;
 
-void InsertionSort(std::vector<int>& arr)
+void InsertionSort(std::vector<double>& arr)
 {
 	for (int i = 1; i < arr.size(); i++) {
 		for (int j = i; j >= 1; j--) {
@@ -14,16 +16,16 @@ void InsertionSort(std::vector<int>& arr)
 
 int main(void)
 {
-	int n = 10;
-	// n개의 버킷 생성
-	std::vector<int>* bucket = new std::vector<int>[n];
+	int n = 11;
+	// n개의 버킷 생성, 각 
+	std::vector<double>* bucket = new std::vector<double>[n];
 
-	int arr[10] = { 0.78,0.17,0.39,0.26,0.72,0.94,0.21,0.12,0.23,0.68 };
-	int arr2[10] = { 1922,2011,8144,1333,9225,3317,5314,1411,1001,9120 };
-
+	// index 1부터 시작
+	double arr[12] = { 0,0.78,0.17,0.39,0.26,0.72,0.94,0.21,0.12,0.23,0.68,0.01 };
+	
 	// 각 숫자에 맞는 자리의 버킷에 넣는다. 기준은 소수점 이하 첫번째 자리
-	for (int i = 0; i < n; i++) 
-		bucket[arr2[i]/1000].push_back(arr2[i]);
+	for (int i = 1; i <= n; i++)
+		bucket[(int)floor(n*arr[i])].push_back(arr[i]);
 	// 각 자리수에 대해서 Insertion sort를 하여 정렬한다.
 	for (int i = 0; i < n; i++)
 		InsertionSort(bucket[i]);
